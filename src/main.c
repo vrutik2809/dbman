564668f0d6480a2489136468fe9abfb29c10892f
Harshpatel2910 <77285337+Harshpatel2910@users.noreply.github.com>
2022-04-10 11:26:02 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,106 +1,145 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "utils.h"
-#include "tables.h"
 #define SIZE 1000
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    char *fields[] = {"id","name","age"};
-    int len = sizeof(fields)/sizeof(*fields);
-    Table t;
-    create_table_struct(argv[1],argv[2],fields,len,&t);
-    print_table_struct(&t);
-    free_table_struct(&t);
     int sts = 0;
+    printf("\n");
+    printf("\033[0;33m"); // yellow color
+
+    printf("There are following commands:\n");
+
+    printf("\033[0m"); // default colour
+
+    printf("\033[0;36m"); // cyan color
+    printf("      1> create db\n");
+    printf("      2> create table\n");
+    printf("      3> insert fields\n");
+    printf("      4> insert values\n");
+    printf("      5> insert fields\n");
+    printf("      6> fetch\n");
+    printf("      7> update db\n");
+    printf("      8> update table\n");
+    printf("      9> update values\n");
+    printf("      10> delete db\n");
+    printf("      11> delete table\n");
+    printf("      12> delete values\n");
+    printf("      13> exit\n");
+    printf("\033[0m"); // default colour
+    printf("\n");
+
     for (;;)
     {
-        printf("\033[0;32m"); //green colour
+        printf("\033[0;32m"); // green colour
         printf("dbman$ ");
-        printf("\033[0m"); //default colour
+        printf("\033[0m"); // default colour
         char cmd_str[SIZE];
-        fgets(cmd_str,SIZE,stdin);
+        fgets(cmd_str, SIZE, stdin);
         cmd_str[strlen(cmd_str) - 1] = '\0';
         char *cmd_arr[SIZE];
-        int idx = split_string(cmd_str," ",cmd_arr);
-        if(strcmp(cmd_arr[0],"create") == 0){
-            if(idx == 1){
+        int idx = split_string(cmd_str, " ", cmd_arr);
+        if (strcmp(cmd_arr[0], "create") == 0)
+        {
+            if (idx == 1)
+            {
                 printf("command not found\n");
             }
-            else if(strcmp(cmd_arr[1],"db") == 0){
+            else if (strcmp(cmd_arr[1], "db") == 0)
+            {
                 // create db logic
             }
-            else if(strcmp(cmd_arr[1],"table") == 0){
+            else if (strcmp(cmd_arr[1], "table") == 0)
+            {
                 // create table logic
             }
-            else{
+            else
+            {
                 printf("command not found\n");
             }
         }
-        else if(strcmp(cmd_arr[0],"insert") == 0){
-            if(idx == 1){
+        else if (strcmp(cmd_arr[0], "insert") == 0)
+        {
+            if (idx == 1)
+            {
                 printf("command not found\n");
             }
-            else if(strcmp(cmd_arr[1],"fields") == 0){
+            else if (strcmp(cmd_arr[1], "fields") == 0)
+            {
                 // insert field logic
             }
-            else if(strcmp(cmd_arr[1],"values") == 0){
+            else if (strcmp(cmd_arr[1], "values") == 0)
+            {
                 // insert value logic
             }
-            else{
+            else
+            {
                 printf("command not found\n");
             }
         }
-        else if(strcmp(cmd_arr[0],"fetch") == 0){
+        else if (strcmp(cmd_arr[0], "fetch") == 0)
+        {
             // fetch logic
         }
-        else if(strcmp(cmd_arr[0],"update") == 0){
-            if(idx == 1){
+        else if (strcmp(cmd_arr[0], "update") == 0)
+        {
+            if (idx == 1)
+            {
                 printf("command not found\n");
             }
-            else if(strcmp(cmd_arr[1],"db") == 0){
+            else if (strcmp(cmd_arr[1], "db") == 0)
+            {
                 // update db logic
             }
-            else if(strcmp(cmd_arr[1],"table") == 0){
+            else if (strcmp(cmd_arr[1], "table") == 0)
+            {
                 // update table logic
             }
-            else if(strcmp(cmd_arr[1],"values") == 0){
+            else if (strcmp(cmd_arr[1], "values") == 0)
+            {
                 // update values logic
-                if(update_values(&t,atoi(cmd_arr[4]),&cmd_arr[4],idx - 4)){
-                    printf("record updated successfully\n");
-                }
             }
-            else{
+            else
+            {
                 printf("command not found\n");
             }
         }
-        else if(strcmp(cmd_arr[0],"delete") == 0){
-            if(idx == 1){
+        else if (strcmp(cmd_arr[0], "delete") == 0)
+        {
+            if (idx == 1)
+            {
                 printf("command not found\n");
             }
-            else if(strcmp(cmd_arr[1],"db") == 0){
+            else if (strcmp(cmd_arr[1], "db") == 0)
+            {
                 // delete db logic
             }
-            else if(strcmp(cmd_arr[1],"table") == 0){
+            else if (strcmp(cmd_arr[1], "table") == 0)
+            {
                 // delete table logic
             }
-            else if(strcmp(cmd_arr[1],"values") == 0){
+            else if (strcmp(cmd_arr[1], "values") == 0)
+            {
                 // delete values logic
             }
-            else{
+            else
+            {
                 printf("command not found\n");
             }
         }
-        else if(strcmp(cmd_arr[0],"exit") == 0){
+        else if (strcmp(cmd_arr[0], "exit") == 0)
+        {
             sts = 1;
         }
-        else{
+        else
+        {
             printf("command not found\n");
         }
 
-        free_str_arr(cmd_arr,idx);
-        if(sts){
+        free_str_arr(cmd_arr, idx);
+        if (sts)
+        {
             break;
         }
     }
