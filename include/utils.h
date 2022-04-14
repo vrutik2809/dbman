e0991fb69278ada8776eefe4660c05daafb790a5
Harshpatel2910 <77285337+Harshpatel2910@users.noreply.github.com>
2022-04-14 14:49:03 +0530

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -4,4 +4,7 @@ void free_str_arr(char *arr[],int idx);
 int is_db_exists_and_valid(char *db_name);
 int is_table_exists_and_valid(char *db_name,char *table_name);
 void make_table_path(char *db_name,char *table_name,char *table_path);
+int delete_table_path(char *db_name,char *table_name);
 void make_db_path(char *db_name,char *db_path);
+int delete_db_path(char *db_name);
+void help(); // to display all the available features
